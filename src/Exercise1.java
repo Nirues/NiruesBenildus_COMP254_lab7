149122b284aeac95f2c3f45ad6acb5167f12ee76
Nirues <nirues225@gmail.com>
2024-04-12 22:57:46 -0400

    Exercise 1 and 2 of lab 07

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,2 +1,52 @@
-package PACKAGE_NAME;public class Exercise1 {
+import java.util.Stack;
+
+public class Exercise1 {
+
+    public static <E> boolean treeSearch(Node<E> root, E target) {
+        if (root == null) {
+            return false;
+        }
+
+        Stack<Node<E>> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()) {
+            Node<E> current = stack.pop();
+            if (current.data.equals(target)) {
+                return true;
+            }
+            if (current.right != null) {
+                stack.push(current.right);
+            }
+            if (current.left != null) {
+                stack.push(current.left);
+            }
+        }
+
+        return false;
+    }
+
+    static class Node<E> {
+        E data;
+        Node<E> left, right;
+
+        public Node(E data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        Node<Integer> root = new Node<>(2);
+        root.left = new Node<>(4);
+        root.right = new Node<>(6);
+        root.left.left = new Node<>(8);
+        root.left.right = new Node<>(10);
+
+
+        System.out.println(treeSearch(root, 8));
+        System.out.println(treeSearch(root, 12));
+    }
 }
+
--- src/Exercise2.java
+++ src/Exercise2.java
@@ -1,2 +1,54 @@
-package PACKAGE_NAME;public class Exercise2 {
+import java.util.*;
+
+public class Exercise2 {
+
+    public static <E extends Comparable<E>> Queue<E> bottomUpMergeSort(Collection<E> collection) {
+        Queue<Queue<E>> queues = new LinkedList<>();
+
+        for (E item : collection) {
+            Queue<E> queue = new LinkedList<>();
+            queue.offer(item);
+            queues.offer(queue);
+        }
+
+
+        while (queues.size() > 1) {
+            Queue<E> queue1 = queues.poll();
+            Queue<E> queue2 = queues.poll();
+            Queue<E> mergedQueue = mergeQueues(queue1, queue2);
+            queues.offer(mergedQueue);
+        }
+
+
+        return queues.poll();
+    }
+
+    private static <E extends Comparable<E>> Queue<E> mergeQueues(Queue<E> queue1, Queue<E> queue2) {
+        Queue<E> mergedQueue = new LinkedList<>();
+
+
+        while (!queue1.isEmpty() && !queue2.isEmpty()) {
+            if (queue1.peek().compareTo(queue2.peek()) <= 0) {
+                mergedQueue.offer(queue1.poll());
+            } else {
+                mergedQueue.offer(queue2.poll());
+            }
+        }
+
+        mergedQueue.addAll(queue1);
+        mergedQueue.addAll(queue2);
+
+        return mergedQueue;
+    }
+
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(9, 4, 7, 2, 5, 1, 8, 3, 6);
+        Queue<Integer> sortedQueue = bottomUpMergeSort(list);
+
+        System.out.println("The sorted elements are as followed :");
+        while (!sortedQueue.isEmpty()) {
+            System.out.print(sortedQueue.poll() + " ");
+        }
+        System.out.println();
+    }
 }
